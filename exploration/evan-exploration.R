1565364710ea91a3125ab59ad5ef755f29ab594f
evandragich <evandragich@gmail.com>
2021-11-08 21:21:10 -0500

    changed to 3d array paradigm for image; functional app!

--- exploration/evan-exploration.R
+++ exploration/evan-exploration.R
@@ -22,19 +22,21 @@ my_colors <- colordistance::getKMeanColors(img, lower = NULL, upper = NULL,
                                            sample.size = FALSE, n = n_color)
 
 # this is the "big boy" vector with all pixels and their mapping
-cluster_vector <- my_colors$cluster
+raw_image_vector <- my_colors$cluster
 
-# grab rgb values of cluster centers to feed into image later
-cluster_centers <- my_colors$centers %>%
-  as_tibble() %>%
-  mutate(id = row_number(),
-         rgb_scaled = rgb(r, g, b)) %>%
-  select(rgb_scaled) %>%
-  pull()
+# # grab rgb values of cluster centers to feed into image later
+# cluster_centers <- my_colors$centers %>%
+#   as_tibble() %>%
+#   mutate(id = row_number(),
+#          rgb_scaled = rgb(r, g, b)) %>%
+#   select(rgb_scaled) %>%
+#   pull()
 
-# get quantified goodness of fit; unrelated to rest of analysis but could be fun to display
-my_r_squared <- ((1 - (my_colors$tot.withinss / my_colors$totss)) * 100) %>%
-  round(digits = 1)
+# NEW Version OF THE ABOVE
+
+cluster_lookup <- my_colors %>%
+  extractClusters() %>%
+  mutate(cluster = row_number())
 
 # gets dimensions of image to help size matrix
 # irrelevant right now with our square images, but will work in future i think
@@ -42,10 +44,25 @@ my_dim <- img %>%
   magick::image_read() %>%
   magick::image_info()
 
+# create 3d image array (width * height * 3 (RGB) channels)
+cluster_map <- tibble(cluster = raw_image_vector) %>%
+  left_join(cluster_lookup)
+
+image_array <- c(cluster_map$R, cluster_map$G, cluster_map$B) %>%
+  array(c(my_dim$height, my_dim$width, 3))
+
+img_yuh <- image_read(image_array)
+
+# get quantified goodness of fit; unrelated to rest of analysis but could be fun to display
+my_r_squared <- ((1 - (my_colors$tot.withinss / my_colors$totss)) * 100) %>%
+  round(digits = 1)
+
+
+
 # need to rotate here because image() has annoying behavior of transposing/reversing rows
 my_matrix <- matrix(cluster_vector, nrow = my_dim$height, ncol = my_dim$width) %>%
   rotateFixed(90)
 
 # combine it all together into our color-simplified raster image!
-my_image <- image(z = my_matrix, col = cluster_centers, axes=FALSE)
+image(z = my_matrix, col = cluster_centers, axes=FALSE)
 
--- pbnify/app.R
+++ pbnify/app.R
@@ -18,123 +18,122 @@ library(colordistance) # eveyrthing else
 # Define UI
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("PBN-ify"),
+  # Application title
+  titlePanel("PBN-ify"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("clusters",
-                        "Number of colors:\n(Most images look best with 3-5!)",
-                        min = 1,
-                        max = 10,
-                        value = 5),
-            fileInput("upload",
-                      "Upload an image:",
-                      accept = "image/*",
-                      placeholder = "No image selected")
-        ),
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("clusters",
+        "Number of colors:\n(Most images look best with 3-5!)",
+        min = 1,
+        max = 10,
+        value = 5
+      ),
+      fileInput("upload",
+        "Upload an image:",
+        accept = "image/*",
+        placeholder = "No image selected"
+      )
+    ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           # plotOutput("distPlot")
-            imageOutput("pixelated_img"),
-            plotOutput("colorspace_plot"),
-            textOutput("r_squared")
-        )
+    # Show a plot of the generated distribution
+    mainPanel(
+      # plotOutput("distPlot")
+      imageOutput("pixelated_img"),
+      plotOutput("colorspace_plot"),
+      textOutput("r_squared")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    #
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
+  # output$distPlot <- renderPlot({
+  #     # generate bins based on input$bins from ui.R
+  #     x    <- faithful[, 2]
+  #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  #
+  #     # draw the histogram with the specified number of bins
+  #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  # })
 
-    # from https://gist.github.com/jeroen/bd1d0a30e7184a5320695ee2bda12c70
-    # observeEvent(input$upload, {
-    #
-    #     image_path <- input$upload$datapath
-    #      image <- image_read(image_path)
-    #     image_info <- image_info(image)
-    # })
+  # from https://gist.github.com/jeroen/bd1d0a30e7184a5320695ee2bda12c70
+  # observeEvent(input$upload, {
+  #
+  #     image_path <- input$upload$datapath
+  #      image <- image_read(image_path)
+  #     image_info <- image_info(image)
+  # })
 
 
 
-    # turns out there was an implicit "sample size = 20000" argument! i was like how is the pixel
-    # count of a square image not a perfect square LOL. way slower now but still good!
-    my_colors <- reactive(
-        colordistance::getKMeanColors(input$upload$datapath, lower = NULL, upper = NULL,
-                                      sample.size = FALSE, n = input$clusters)
+  # turns out there was an implicit "sample size = 20000" argument! i was like how is the pixel
+  # count of a square image not a perfect square LOL. way slower now but still good!
+  my_colors <- reactive(
+    colordistance::getKMeanColors(input$upload$datapath,
+      lower = NULL, upper = NULL,
+      sample.size = FALSE, n = input$clusters
     )
+  )
 
-    # # this is the "big boy" vector with all pixels and their mapping
-    # cluster_vector <- reactive(
-    #     my_colors$cluster
-    # )
+  # grab rgb values of cluster centers to feed into image later
+  # this is a df with obs for every cluster, cols RGB vals and % of image
+  cluster_lookup <- reactive(
+    my_colors() %>%
+      extractClusters() %>%
+      mutate(cluster = row_number())
+  )
 
-    # grab rgb values of cluster centers to feed into image later
-    clustered_colors <- reactive(
-        my_colors()$centers %>%
-            as_tibble() %>%
-            mutate(id = row_number(),
-                   rgb_scaled = rgb(r, g, b)) %>%
-            select(rgb_scaled) %>%
-            pull()
+  # get quantified goodness of fit; unrelated to rest of analysis but could be fun to display
+  output$r_squared <- renderText({
+    value <- ((1 - (my_colors()$tot.withinss / my_colors()$totss)) * 100) %>%
+      round(digits = 1)
+    paste0("The goodness of fit, or R-Squared, associated with this pixelation is ", value, "%")
+  })
+
+
+  # obtains dimensions of original image to size first 2d of 3d array
+  dim <- reactive(
+    input$upload$datapath %>%
+      image_read() %>%
+      image_info()
+  )
+
+  # parses the cluster vector outputted from getKmeanColors() with
+  # cluster lookup table to create 3d array of pixelated image!
+
+  pxl_img_array <- reactive(
+    tibble(cluster = my_colors()$cluster) %>%
+      left_join(cluster_lookup()) %>%
+      {
+        array(
+          data = c(.$R, .$G, .$B),
+          dim = c(dim()$height, dim()$width, 3)
+        )
+      }
+  )
+
+
+  output$pixelated_img <- renderImage(
+    {
+      ret <- pxl_img_array() %>%
+        image_read() %>%
+        image_write(tempfile(fileext = "jpeg"), format = "jpeg")
+
+
+      list(src = ret, contentType = "image/jpeg")
+    },
+    deleteFile = FALSE
+  )
+
+  output$colorspace_plot <- renderPlot({
+    colordistance::plotPixels(input$upload$datapath,
+      n = TRUE,
+      lower = NULL, upper = NULL
     )
-
-    # get quantified goodness of fit; unrelated to rest of analysis but could be fun to display
-    output$r_squared <- renderText({
-        value <- ((1 - (my_colors()$tot.withinss / my_colors()$totss)) * 100) %>%
-            round(digits = 1)
-        paste0("The goodness of fit, or R-Squared, associated with this pixelation is ", value, "%")
-    })
-
-    # gets dimensions of image to help size matrix
-    # irrelevant right now with our square images, but will work in future i think
-    # metadata <- img %>%
-    #     magick::image_read() %>%
-    #     magick::image_info()
-
-    # metadata <- reactive(
-    #     img_path() %>%
-    #         magick::image_read() %>%
-    #         magick::image_info()
-    # )
-
-    dim <- reactive(
-        input$upload$datapath %>%
-            image_read() %>%
-            image_info()
-    )
-
-    # need to rotate here because image() has annoying behavior of transposing/reversing rows
-    img_matrix <- reactive(
-        matrix(my_colors()$cluster, nrow = dim()$height, ncol = dim()$width) %>%
-            rotateFixed(90)
-    )
-
-
-    output$pixelated_img <- renderImage({
-        # combine it all together into our color-simplified raster image!
-        ret <- image(z = img_matrix(), col = clustered_colors(), axes=FALSE) %>%
-            as.raster() %>%
-            image_read() %>%
-            image_write(tempfile(fileext = 'jpeg'), format = 'jpeg')
-
-
-        list(src = ret, contentType = "image/jpeg")
-    })
-
-    output$colorspace_plot <- renderPlot({
-        colordistance::plotPixels(input$upload$datapath, lower = NULL, upper = NULL)
-    })
+  })
 }
 
 # Run the application
