56263883cd74ca45ece94adf5f10f2341c110bbd
evandragich <evandragich@gmail.com>
2021-11-17 14:19:05 -0500

    added conditional text color in reactable; styled

--- app.R
+++ app.R
@@ -3,7 +3,6 @@ library(shiny)
 library(tidyverse)
 library(scales) # label_percent on degree base plot
 library(reactable) # color table
-library(reactablefmtr) # conditional colors in cells
 library(magick) # generalize beyond jpeg files
 # magick will also help with flood filling later
 library(palmerpenguins) # penguins dataset for sample plot
@@ -16,7 +15,7 @@ library(colorblindr) # okabe-ito palette for sample plots
 # dog_travel: https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-12-17
 # sample plots inspired by HW4 Q1
 # source for color names:
-#https://en.wikipedia.org/wiki/Lists_of_colors
+# https://en.wikipedia.org/wiki/Lists_of_colors
 
 
 # https://cran.r-project.org/web/packages/magick/vignettes/intro.html#Cut_and_edit
@@ -28,7 +27,7 @@ source("exploration/outline.R")
 degrees <- read_csv(here::here("data", "BA_degrees.csv")) %>%
   filter(year >= 1990)
 
-dog_travel <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_travel.csv')
+dog_travel <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_travel.csv")
 state_list <- tibble(abbreviation = state.abb, name = state.name)
 
 # load color name data
@@ -49,8 +48,10 @@ ui <- fluidPage(
 
   # Application title
   titlePanel(
-    h1("PBN-ify",
-    h4("Click \"Browse...\" to replace the default image with one of your own"))
+    h1(
+      "PBN-ify",
+      h4("Click \"Browse...\" to replace the default image with one of your own")
+    )
   ),
   tabsetPanel(
     tabPanel(
@@ -59,15 +60,15 @@ ui <- fluidPage(
       sidebarLayout(
         sidebarPanel(
           sliderInput("clusters",
-                      "Number of colors:\n(Most images look best with 3-5!)",
-                      min = 1,
-                      max = 10,
-                      value = 5
+            "Number of colors:\n(Most images look best with 3-5!)",
+            min = 1,
+            max = 10,
+            value = 5
           ),
           fileInput("upload",
-                    "Upload an image:",
-                    accept = "image/*",
-                    placeholder = "No image selected"
+            "Upload an image:",
+            accept = "image/*",
+            placeholder = "No image selected"
           )
         ),
 
@@ -86,31 +87,30 @@ ui <- fluidPage(
             tabPanel(
               title = "Simplified Output",
               imageOutput("pixelated_img", height = "200px"),
-              downloadButton('download_pxl', 'Download modified image')
+              downloadButton("download_pxl", "Download modified image")
             ),
             tabPanel(
               title = "Outline",
               plotOutput("outline")
             )
-
           )
         )
-        )
-      ),
+      )
+    ),
     tabPanel(
       title = "Color Palette",
       textOutput("example_plot_description"),
       radioButtons("example_type",
-                   "Choose Plot Type:",
-                   choices = c("Discrete", "Sequential", "Diverging"),
-                   selected = "Discrete"
+        "Choose Plot Type:",
+        choices = c("Discrete", "Sequential", "Diverging"),
+        selected = "Discrete"
       ),
       sliderInput("gray_val",
-                  "Adjust the darkness of the \"Other\" or NA Category",
-                  min = 10,
-                  max = 90,
-                  value = 30,
-                  step = 5
+        "Adjust the darkness of the \"Other\" or NA Category",
+        min = 10,
+        max = 90,
+        value = 30,
+        step = 5
       ),
       h2("PBN-ified Plot"),
       plotOutput("colorized_plot"),
@@ -120,9 +120,8 @@ ui <- fluidPage(
       plotOutput("viridis_plot"),
       h2("Okabe-Ito Plot"),
       plotOutput("okabeito_plot")
-      )
-
-)
+    )
+  )
 )
 
 # Define server logic
@@ -158,7 +157,6 @@ server <- function(input, output) {
       pull()
 
     temp[1]
-
   }
 
   # grab rgb values of cluster centers to feed into image later
@@ -216,14 +214,13 @@ server <- function(input, output) {
   # write image object. needs outside of pixelated_img for use when downloading
   ret <- reactive(
     pxl_img_array() %>%
-    image_read() %>%
-    image_write(tempfile(fileext = my_dim()$format), format = my_dim()$format)
+      image_read() %>%
+      image_write(tempfile(fileext = my_dim()$format), format = my_dim()$format)
   )
 
 
   output$pixelated_img <- renderImage(
     {
-
       list(src = ret(), contentType = paste0("image/", my_dim()$format), height = "200px")
     },
     # saves image after sending to UI
@@ -239,41 +236,49 @@ server <- function(input, output) {
 
   find_closest <- function(my_r, my_g, my_b) {
     temp <- color_names %>%
-      mutate(diff = abs(R - my_r) + abs(G - my_g) + abs(B - my_b)) %>%
+      mutate(diff = (R - my_r)^2 + (G - my_g)^2 + (B - my_b)^2) %>%
       arrange(diff) %>%
       select(name) %>%
       pull()
 
     temp[1]
+  }
 
+  hex_to_bw <- function(hex) {
+    temp <- col2rgb(hex)[1, 1]^2 * .241 +
+      col2rgb(hex)[2, 1]^2 * .691 +
+      col2rgb(hex)[3, 1]^2 * .068
+
+    ret <- if_else(temp > 16900, "#000000", "#FFFFFF")
+    ret
   }
 
   output$color_table <- renderReactable({
-    cluster_lookup() %>%
-
+    temp <- cluster_lookup() %>%
       # works now, but minimal matching. will work on matching closest neighbor
-      #left_join(color_names, by = "rgb_scaled") %>%
+      # left_join(color_names, by = "rgb_scaled") %>%
       arrange(desc(Pct)) %>%
       mutate(
-        Pct = paste0(round(Pct * 100, digits = 1), "%"),
-        placeholder = NA
+        Pct = paste0(round(Pct * 100, digits = 1), "%")
       ) %>%
-      select(rgb_scaled, Pct, placeholder, closest) %>%
-      reactable(
-        columns = list(
-          rgb_scaled = colDef(name = "Hex Code"),
-          Pct = colDef(name = "Percentage of pixels"),
-           closest = colDef(name = "Nearest named color"),
-          placeholder = colDef(
-            name = "Color",
-            style = function(value, index) {
-              color <- cluster_lookup()$rgb_scaled[index]
-              list(background = color)
-            }
-          )
+      select(rgb_scaled, Pct, closest)
+
+    reactable(temp,
+      columns = list(
+        Pct = colDef(name = "Percentage of pixels"),
+        rgb_scaled = colDef(
+          name = "Hex Code",
+          style = function(value) {
+            list(
+              color = hex_to_bw(value),
+              background = value
+            )
+          }
         ),
-        rownames = FALSE,
-      )
+        closest = colDef(name = "Nearest named color")
+      ),
+      rownames = FALSE
+    )
   })
 
   # description to explain example plot tab
@@ -284,23 +289,23 @@ server <- function(input, output) {
   # create base discrete plot to be layered upon
   # only reactive change is number of non-lumped factors
   discrete_plot <- reactive(
-
     degrees %>%
       arrange(desc(perc)) %>%
-    mutate(field = fct_other(field,
-                              keep = ordered_fields[1:input$clusters])) %>%
-    group_by(field, year) %>%
-    summarise(perc = sum(perc), .groups = "drop_last") %>%
-    ggplot(mapping = aes(x = year, y = perc, group = field, color = field)) +
-    geom_line() +
-    labs(
-      title = "Percentage of annual degrees awarded",
-      subtitle = "from 1990 to 2015 in the US",
-      x = "Year",
-      y = "Percent",
-      color = "Field"
-    ) +
-    scale_y_continuous(labels = label_percent())  +
+      mutate(field = fct_other(field,
+        keep = ordered_fields[1:input$clusters]
+      )) %>%
+      group_by(field, year) %>%
+      summarise(perc = sum(perc), .groups = "drop_last") %>%
+      ggplot(mapping = aes(x = year, y = perc, group = field, color = field)) +
+      geom_line() +
+      labs(
+        title = "Percentage of annual degrees awarded",
+        subtitle = "from 1990 to 2015 in the US",
+        x = "Year",
+        y = "Percent",
+        color = "Field"
+      ) +
+      scale_y_continuous(labels = label_percent()) +
       theme_minimal() +
       theme(legend.position = "top")
   )
@@ -309,99 +314,99 @@ server <- function(input, output) {
   sequential_plot <- dog_travel %>%
     group_by(contact_state) %>%
     summarise(n = n()) %>%
-  right_join(state_list, by = c("contact_state" = "abbreviation")) %>%
+    right_join(state_list, by = c("contact_state" = "abbreviation")) %>%
     mutate(name = str_to_lower(name)) %>%
     left_join(map_data("state"), dogs, by = c("name" = "region")) %>%
     ggplot(mapping = aes(x = long, y = lat, group = group, fill = n)) +
     geom_polygon(color = "black") +
-    labs(title = "Number of Dogs Available to Adopt in U.S. States",
-         caption = "Gray represents states with missing or unknown data.",
-         fill = "Dogs") +
+    labs(
+      title = "Number of Dogs Available to Adopt in U.S. States",
+      caption = "Gray represents states with missing or unknown data.",
+      fill = "Dogs"
+    ) +
     theme_void() +
     theme(plot.title = element_text(size = 18, hjust = 0.5))
 
-## EVAN TODO:
+  ## EVAN TODO:
   # create sample plot for diverging
   # create UI to choose color for diverging
 
 
-# output the base ggplot for discrete color palettes
-    output$basic_plot <- renderPlot({
-      if (input$example_type == "Discrete") {
+  # output the base ggplot for discrete color palettes
+  output$basic_plot <- renderPlot({
+    if (input$example_type == "Discrete") {
       discrete_plot() +
         scale_color_manual(
           values = c(hue_pal()(input$clusters), paste0("gray", (100 - input$gray_val))),
         )
-      } else if (input$example_type == "Sequential") {
-        sequential_plot +
-          scale_fill_gradient(trans = "log10",
-                              na.value = paste0("gray", (100 - input$gray_val)))
-      }
-
+    } else if (input$example_type == "Sequential") {
+      sequential_plot +
+        scale_fill_gradient(
+          trans = "log10",
+          na.value = paste0("gray", (100 - input$gray_val))
+        )
+    }
   })
 
-# output the ggplot with our colors for discrete color palettes
-output$colorized_plot <- renderPlot({
-  if (input$example_type == "Discrete") {
-  discrete_plot() +
-    scale_color_manual(
-      values = c(cluster_lookup()$rgb_scaled, paste0("gray", (100 - input$gray_val))),
-    )
-  } else if (input$example_type == "Sequential") {
-    sequential_plot +
-      scale_fill_gradient(trans = "log10",
-                          low = ordered_hexes()[1],
-                          high = ordered_hexes()[input$clusters],
-                          na.value = paste0("gray", (100 - input$gray_val)))
-  }
+  # output the ggplot with our colors for discrete color palettes
+  output$colorized_plot <- renderPlot({
+    if (input$example_type == "Discrete") {
+      discrete_plot() +
+        scale_color_manual(
+          values = c(cluster_lookup()$rgb_scaled, paste0("gray", (100 - input$gray_val))),
+        )
+    } else if (input$example_type == "Sequential") {
+      sequential_plot +
+        scale_fill_gradient(
+          trans = "log10",
+          low = ordered_hexes()[1],
+          high = ordered_hexes()[input$clusters],
+          na.value = paste0("gray", (100 - input$gray_val))
+        )
+    }
+  })
 
-})
+  # output the ggplot with okabe ito colors for discrete color palettes
+  # fyi -- OkabeIto can only take 7 non-gray values, so will need guard
+  output$okabeito_plot <- renderPlot({
+    if (input$example_type == "Discrete") {
+      discrete_plot() +
+        scale_color_manual(
+          values = c(palette_OkabeIto[1:input$clusters], paste0("gray", (100 - input$gray_val))),
+        )
+    } else if (input$example_type == "Sequential") {
+      NULL
+    }
+  })
 
-# output the ggplot with okabe ito colors for discrete color palettes
-# fyi -- OkabeIto can only take 7 non-gray values, so will need guard
-output$okabeito_plot <- renderPlot({
-  if (input$example_type == "Discrete") {
-  discrete_plot() +
-    scale_color_manual(
-      values = c(palette_OkabeIto[1:input$clusters], paste0("gray", (100 - input$gray_val))),
-    )
-} else if (input$example_type == "Sequential") {
-  NULL
-}
-
-})
-
-# output the ggplot with viridis colors for discrete color palettes
-output$viridis_plot <- renderPlot({
-  if (input$example_type == "Discrete") {
-  discrete_plot() +
-    scale_color_manual(
-      values = c(viridis_pal()(input$clusters), paste0("gray", (100 - input$gray_val))),
-    )
-  } else if (input$example_type == "Sequential") {
-    sequential_plot +
-      scale_fill_viridis_c(trans = "log10",
-                           option = "A",
-                           na.value = paste0("gray", (100 - input$gray_val)))
-  }
-
-})
+  # output the ggplot with viridis colors for discrete color palettes
+  output$viridis_plot <- renderPlot({
+    if (input$example_type == "Discrete") {
+      discrete_plot() +
+        scale_color_manual(
+          values = c(viridis_pal()(input$clusters), paste0("gray", (100 - input$gray_val))),
+        )
+    } else if (input$example_type == "Sequential") {
+      sequential_plot +
+        scale_fill_viridis_c(
+          trans = "log10",
+          option = "A",
+          na.value = paste0("gray", (100 - input$gray_val))
+        )
+    }
+  })
 
   output$outline <- renderPlot({
     outline_func(my_colors()$cluster, c(my_dim()$height, my_dim()$width))
-
-})
+  })
 
   output$download_pxl <- downloadHandler(
-    filename = paste0("pixelated_image_", input$clusters, "_colors_", Sys.Date(),".jpeg", sep = ''),
+    filename = paste0("pixelated_image_", input$clusters, "_colors_", Sys.Date(), ".jpeg", sep = ""),
     contentType = "image/jpeg",
     content = function(file) {
       file.copy(ret(), file)
     }
   )
-
-
-
 }
 
 # Run the application
